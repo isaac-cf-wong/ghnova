ed84863e67d18fa603ecd43d4d1c177f7281424b
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-18 12:00:49 +0100

    chore: Add the following functions to the public API
    - `process_response_with_last_modified`
    - `process_async_response_with_last_modified`

--- src/ghnova/utils/__init__.py
+++ src/ghnova/utils/__init__.py
@@ -3,5 +3,14 @@
 from __future__ import annotations
 
 from ghnova.utils.log import get_version_information, setup_logger
+from ghnova.utils.response import (
+    process_async_response_with_last_modified,
+    process_response_with_last_modified,
+)
 
-__all__ = ["get_version_information", "setup_logger"]
+__all__ = [
+    "get_version_information",
+    "process_async_response_with_last_modified",
+    "process_response_with_last_modified",
+    "setup_logger",
+]

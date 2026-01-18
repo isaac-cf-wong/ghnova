2fecf78a71571df5ce396ccddeb0e65188b3674b
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-18 05:40:02 +0100

    chore: Expose `client` module to the public API

--- src/ghnova/__init__.py
+++ src/ghnova/__init__.py
@@ -4,6 +4,7 @@ Top-level package for ghnova.
 
 from __future__ import annotations
 
+from ghnova import client
 from ghnova.version import __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "client"]

64a03a4e2232aa585800f56235320073a524b443
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-01-19 13:45:14 +0100

    feat: Add `Issue` to the `GitHub` client

--- src/ghnova/client/github.py
+++ src/ghnova/client/github.py
@@ -8,6 +8,7 @@ import requests
 from requests import Response
 
 from ghnova.client.base import Client
+from ghnova.issue.issue import Issue
 from ghnova.user.user import User
 
 
@@ -23,6 +24,7 @@ class GitHub(Client):
         """
         super().__init__(token=token, base_url=base_url)
         self.session: requests.Session | None = None
+        self.issue = Issue(client=self)
         self.user = User(client=self)
 
     def __str__(self) -> str:

6b2a3b5aef4c7f8d0828a9a2db80c53d7131df09
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-02-04 04:38:30 +0100

    chore: Expose `execute_api_command` to public API

--- src/ghnova/cli/utils/__init__.py
+++ src/ghnova/cli/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from ghnova.cli.utils.api import execute_api_command
 from ghnova.cli.utils.auth import get_auth_params
 
-__all__ = ["get_auth_params"]
+__all__ = ["execute_api_command", "get_auth_params"]
